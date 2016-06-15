b0e7aa83dbed19212e9dbb1451b576cc370e18aa
Gabe Harms <gabeharms@gmail.com>
2016-06-15 09:14:01 -0400

    Initial Commit of Rust Backend

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate iron;
+extern crate router;
+extern crate rustc_serialize;
+extern crate staticfile;
+extern crate mount;
+
+use iron::prelude::*;
+use iron::status;
+use iron::Url;
+use iron::modifiers::Redirect;
+use router::Router;
+use staticfile::Static;
+use mount::Mount;
+use std::path::Path;
+
+
+
+fn main() {
+
+    fn HomePage(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((iron::status::Ok, "yo")))
+    }
+    fn RedirectHome(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Found,
+                           Redirect(Url::parse("http://localhost:3000/home").unwrap()))))
+    }
+
+    let mut router = Router::new();
+    router
+        .get("/", RedirectHome)
+        .get("/api", HomePage);
+    //router.get("/api/expenses", Expenses);
+
+    let mut mount = Mount::new();
+    mount
+        .mount("/", router)
+        .mount("/home",
+               Static::new(Path::new("/Users/gabeharms/Desktop/Practice/Payment-Processor/index.html")));
+        
+    Iron::new(mount).http("localhost:3000").unwrap();
+}

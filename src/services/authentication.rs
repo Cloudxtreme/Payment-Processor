c571df50d5c409e7a101aaa29d99651ad049ed7a
Gabe Harms <gabeharms@gmail.com>
2016-07-28 15:08:21 -0700

    Setting user id no matter what, just for improved security

--- src/services/authentication.rs
+++ src/services/authentication.rs
@@ -1,5 +1,8 @@
 use iron::prelude::*;
 use iron::BeforeMiddleware;
+use iron::status;
+use std::error::{Error};
+use std::fmt::{self, Debug};
 use util::Auth;
 use services::{verify_token};
 
@@ -14,6 +17,19 @@ pub struct Authentication;
 // Don't forget to blank out whatever is existing in the requests extentions to avoid a
 // vulnerability
 
+#[derive(Debug)]
+struct StringError(String);
+
+impl fmt::Display for StringError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(self, f)
+    }
+}
+
+impl Error for StringError {
+    fn description(&self) -> &str { &*self.0 }
+}
+
 /// Middleware that determines if a user is authenticated already or not. If the inbound request
 /// contains a token that can be decoded properly into a user id, then the user has already been
 /// authenticated, and we will insert the user id into the request's extension member for further
@@ -24,10 +40,17 @@ impl BeforeMiddleware for Authentication {
             String::from_utf8((*req.headers.get_raw("X-Auth").unwrap())[0].clone()).ok().unwrap();
         let user_id = verify_token(&token);
 
+
         req.extensions.insert::<Auth>(user_id);
-        Ok(())  
-            //Err(IronError::new(StringError("Error in ErrorProducer
-            //                                      BeforeMiddleware".to_string()), status::BadRequest))
+        if user_id == -1 {
+            Err(IronError::new(
+                    StringError("Error in ErrorProducer BeforeMiddleware".to_string()),
+                    status::BadRequest)
+               )
+        }
+        else {
+            Ok(())
+        }
 
     }
     fn catch(&self, _: &mut Request, _: IronError) -> IronResult<()> { 
